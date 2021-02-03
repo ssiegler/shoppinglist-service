30d92443e44f21f7034dbc03d13f7109142b861b
Simon Siegler <simon@simon-siegler.de>
2021-02-03 19:13:04 +0100

    Import project generated by spring initializr

---
+++ src/main/java/io/github/ssiegler/demos/shoppinglistservice/ShoppinglistServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.ssiegler.demos.shoppinglistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinglistServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppinglistServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ssiegler/demos/shoppinglistservice/ShoppinglistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ssiegler.demos.shoppinglistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinglistServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
